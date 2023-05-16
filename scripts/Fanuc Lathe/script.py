b713522cc5bf88c61a4a9980633446651e10dcdc
Jeikobu <35476186+Jeikobuka@users.noreply.github.com>
2023-05-16 13:57:10 -0700

    Update script.py

--- scripts/Fanuc Lathe/script.py
+++ scripts/Fanuc Lathe/script.py
@@ -1,15 +1,16 @@
+import os
 # ------------------------------------------------
 converts = {}
 GCODE_FILE = 'swapped_gcodes.txt'
 
 def getConverts(prepend_path):
-    with open(prepend_path+'/'+GCODE_FILE, 'r') as f:
+    with open(prepend_path+'\\'+GCODE_FILE, 'r') as f:
         for line in f.readlines():
             converts[line.split('->')[0].strip()] = line.split('->')[1].strip()
     return converts
 
-def convertGCodes(content, prepend_path):
-    converts = getConverts(prepend_path)
+def convertGCodes(content, scriptPath):
+    converts = getConverts(scriptPath)
     newConvContent = []
     for line in content.split('\n'):
         for convert in converts:
@@ -20,13 +21,8 @@ def convertGCodes(content, prepend_path):
                 break
         newConvContent.append(line)
     return '\n'.join(newConvContent)
-'''
-def changeToolNumFormat(content):
-    for toolNum in range(100):
-        content = content.replace(f"T{toolNum}M6", f"M6T{toolNum}").replace(f"T{toolNum}\nM6", f"M6T{toolNum}")
-    return content
-'''
-def main(content, prepend_path):
-    newContent = convertGCodes(content, prepend_path)
+
+def main(content):
+    newContent = convertGCodes(content, os.path.dirname(__file__))
     #newContent = changeToolNumFormat(newContent)
     return newContent
